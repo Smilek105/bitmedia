6806203d2a6511fca83653faec65f4a3d382af5e
Vitalii <vitalyk2001@rambler.ru>
2021-08-16 00:52:18 +0300

    fix categories/update for REST API

--- models/Categories.php
+++ models/Categories.php
@@ -93,7 +93,10 @@ class Categories extends \yii\db\ActiveRecord
 
     public function beforeSave($insert): bool
     {
-        $this->deleteImage($this->oldAttributes);
+        if($this->attributes['image'] != $this->oldAttributes['image']){
+            $this->deleteImage($this->oldAttributes);
+        }
+
         return parent::beforeSave($insert);
     }
 
