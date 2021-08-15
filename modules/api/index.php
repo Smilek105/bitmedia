db9be9af7f167d5ff65e6502f1ce6083c96dd850
Vitalii <vitalyk2001@rambler.ru>
2021-08-16 00:04:50 +0300

    fix link

--- modules/api/index.php
+++ modules/api/index.php
@@ -4,8 +4,8 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../../vendor/autoload.php');
+require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 
 // Use a distinct configuration for the API
 $config = require(__DIR__ . '/config/api.php');
